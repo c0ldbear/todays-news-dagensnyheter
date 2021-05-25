7a1a96df983ed923bbca40699b156045627d70af
teddy <teddyjh@protonmail.com>
2021-05-25 09:57:48 +0200

    add function to parse the html-code to find the news urls
    We add a function to parse the html-code to find the news urls, add
    these to a list, and then print the elements in the list to the
    terminal.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import aiohttp
-import asyncio
 from bs4 import BeautifulSoup
+import asyncio
 
 async def fetchNews():
     url = "https://www.dn.se/nyhetsdygnet"
@@ -8,9 +8,25 @@ async def fetchNews():
         async with session.get(url) as response:
             rText = await response.text()
             #print(await response.text())
+    return await findNewsTopics(rText)
+    
+
+async def findNewsTopics(responseText):
+    soup = BeautifulSoup(responseText, "html.parser")
+    news_timeline = soup.find("div", attrs={'class':'timeline-page'})
+    news_links = news_timeline.find_all("a", href=True)
+    newsUrls = []
+    for link in news_links:
+        #topic = str(link.find("h3"))
+        a_link = link["href"]
+        newsUrls += [(a_link)]
+    return newsUrls
+
 
 async def main():
-    await fetchNews()
+    News = await fetchNews()
+    for news in News[:-1]:
+        print("https://www.dn.se" + news)
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
