ab3ed5434d30053168bab63d7b5f66f8ce7746b7
teddy <teddyjh@protonmail.com>
2021-05-25 09:46:13 +0200

    add function to fetch the news from dn's nyhetsdygnet

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import aiohttp
 import asyncio
+from bs4 import BeautifulSoup
+
+async def fetchNews():
+    url = "https://www.dn.se/nyhetsdygnet"
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            rText = await response.text()
+            #print(await response.text())
 
 async def main():
-    print("Hello!\nWait 1 sec plz...")
-    await asyncio.sleep(1)
-    print("Now you've waited 1 sec. Congratulations!")
+    await fetchNews()
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
