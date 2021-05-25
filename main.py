739fde89a9f6c63e5c4a85e97faa200cdda67a31
teddy <teddyjh@protonmail.com>
2021-05-25 13:39:30 +0200

    add comments to functions and clean-up code a bit
    Add necessary comments to understand better what the functions does.
    Clean-up the code where we keep the functions simple. One function, one
    thing. Not that a "intermidiate" (mellanhands) function calls another
    function - if not necessary.

--- main.py
+++ main.py
@@ -2,31 +2,55 @@ import aiohttp
 from bs4 import BeautifulSoup
 import asyncio
 
+async def GrabNewsUrls():
+    '''
+    Get the news article links in a form that can be clickable
+    Input:      None
+    Output:     news article links in a clickable form
+    Effect:     None
+    '''
+    return await findNewsTopics(await fetchNews())
+
 async def fetchNews():
+    '''
+    Get the HTML-text/-code from DN.se's webside /nyhetsdygnet
+
+    Input:      None
+    Output:     rText - HTML-text/-code as text
+    Effect:     Get the HTML-text/-code from DN.se's website /nyhetsdygnet as text
+    '''
     url = "https://www.dn.se/nyhetsdygnet"
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
             rText = await response.text()
-            #print(await response.text())
-    return await findNewsTopics(rText)
-    
+    return rText
 
 async def findNewsTopics(responseText):
+    '''
+    Parse the HTML-text/-code to get the links to the news articles.
+    
+    Input:      responseText - the response from 'aiohttp' as text
+    Output:     newsUrlsList - a list with all the links
+    Effect:     parses responseText to find the links and adds them to a list
+    '''
     soup = BeautifulSoup(responseText, "html.parser")
     news_timeline = soup.find("div", attrs={'class':'timeline-page'})
     news_links = news_timeline.find_all("a", href=True)
-    newsUrls = []
+    newsUrlsList = []
     for link in news_links:
-        #topic = str(link.find("h3"))
         a_link = link["href"]
-        newsUrls += [(a_link)]
-    return newsUrls
-
+        newsUrlsList += [a_link]
+    return newsUrlsList
 
 async def main():
-    News = await fetchNews()
+    '''
+    Test script to figure out how to get the urls of the news
+    '''
+    News = await GrabNewsUrls()
+    newsUrlString = ""
     for news in News[:-1]:
-        print("https://www.dn.se" + news)
+        newsUrlString += "https://www.dn.se" + str(news) + "\n"
+    print(newsUrlString)
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
